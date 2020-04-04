3eaf8c2d88dddbc0fe38fd9e5f7d05102e71235f
Felipe Gonçalves <f_g@outlook.com.br>
2020-04-04 12:54:09 -0300

    Alterações Base

---
+++ Primeiro/Primeiro/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+            Pessoa p2 = new Pessoa();
+
+            Console.WriteLine("Insira os dados da Primeira Pessoa: ");
+            Console.Write("Nome: ");
+            p1.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            p1.Idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira os dados da Segunda Pessoa: ");
+            Console.Write("Nome: ");
+            p2.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            p2.Idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\n- Dados das pessoas -\nPrimeira Pessoa:\nNome - {p1.Nome}\nIdade - {p1.Idade}\nSegunda Pessoa:\nNome - {p2.Nome}\nIdade - {p2.Idade}");
+        }
+    }
+}
